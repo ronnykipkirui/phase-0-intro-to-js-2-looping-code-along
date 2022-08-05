9b69f1dfe03a2a5f238a0b277100c85791ae3f08
Ronny <ronnybet41@gmail.com>
2022-08-05 09:00:00 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solutions in this file
+function writeCards(names, surprise){
+    let newArr = []
+    names.forEach(element => {
+        newArr.push(`Thank you, ${element}, for the wonderful ${surprise} gift!`)
+    });
+    return newArr
+}
+function countDown(number){
+    for(let i = 0; i <= number; i++){
+        console.log(i)
+    }
+}
